53b44d3c2beb3c563759b1d92b894d1b914c5a23
Sondre Nilsen <sondre@eons.io>
2021-12-01 14:17:45 +0100

    Eh, lets do it in Haskell

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+
+import qualified AoC (hello)
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  AoC.hello
---
+++ src/AoC.hs
@@ -0,0 +1,4 @@
+module AoC (hello) where
+
+hello :: IO ()
+hello = putStrLn "hello!"
