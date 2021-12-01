2b518b9817a1c6a27636326736e18e9435ecc6c7
Sondre Nilsen <sondre@eons.io>
2021-12-01 17:05:17 +0100

    Create AoC trait for solving days

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,21 +1,25 @@
-fn parse(input: &str) -> Vec<usize> {
-    input.lines().map(|i| i.parse().unwrap()).collect()
-}
+use super::AoC;
 
-fn part_one(input: &[usize]) -> usize {
-    input
-        .windows(2)
-        .map(|w| if w[1] > w[0] { 1 } else { 0 })
-        .sum()
-}
+pub struct Day01;
 
-fn part_two(input: &[usize]) -> usize {
-    part_one(&input.windows(3).map(|w| w.iter().sum()).collect::<Vec<_>>())
-}
+impl AoC for Day01 {
+    type Output = usize;
+    type Input = usize;
 
-pub fn day_one(input: &str) -> (String, String) {
-    let data = parse(input);
-    (part_one(&data).to_string(), part_two(&data).to_string())
+    fn parse(input: &str) -> Vec<usize> {
+        input.lines().map(|i| i.parse().unwrap()).collect()
+    }
+
+    fn part_one(input: &[usize]) -> usize {
+        input
+            .windows(2)
+            .map(|w| if w[1] > w[0] { 1 } else { 0 })
+            .sum()
+    }
+
+    fn part_two(input: &[usize]) -> usize {
+        Self::part_one(&input.windows(3).map(|w| w.iter().sum()).collect::<Vec<_>>())
+    }
 }
 
 #[cfg(test)]
@@ -26,25 +30,25 @@ mod day01 {
 
     #[test]
     fn part_1_example() {
-        let data = parse("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
-        assert_eq!(7, part_one(&data));
+        let data = Day01::parse("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
+        assert_eq!(7, Day01::part_one(&data));
     }
 
     #[test]
     fn part_1() {
-        let data = parse(INPUT);
-        assert_eq!(1529, part_one(&data));
+        let data = Day01::parse(INPUT);
+        assert_eq!(1529, Day01::part_one(&data));
     }
 
     #[test]
     fn part_2_example() {
-        let data = parse("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
-        assert_eq!(5, part_two(&data));
+        let data = Day01::parse("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
+        assert_eq!(5, Day01::part_two(&data));
     }
 
     #[test]
     fn part_2() {
-        let data = parse(INPUT);
-        assert_eq!(1567, part_two(&data));
+        let data = Day01::parse(INPUT);
+        assert_eq!(1567, Day01::part_two(&data));
     }
 }
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,8 +1,25 @@
 mod day01;
 
+trait AoC {
+    type Output: std::fmt::Display;
+    type Input;
+
+    fn parse(input: &str) -> Vec<Self::Input>;
+    fn part_one(input: &[Self::Input]) -> Self::Output;
+    fn part_two(input: &[Self::Input]) -> Self::Output;
+    fn solve(input: &str) -> (String, String) {
+        let data = Self::parse(input);
+
+        (
+            Self::part_one(&data).to_string(),
+            Self::part_two(&data).to_string(),
+        )
+    }
+}
+
 pub fn run_day(day: usize, input: &str) -> (String, String) {
     match day {
-        1 => day01::day_one(input),
+        1 => day01::Day01::solve(input),
         _ => ("Not implemented".to_string(), "Not implemented".to_string()),
     }
 }
