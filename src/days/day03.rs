50c86860ee5faa50425c3d85e76353b721688b1d
Sondre Nilsen <sondre@eons.io>
2021-12-03 12:27:02 +0100

    Day 03, part 2, phew

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -2,6 +2,25 @@ use super::AoC;
 
 pub struct Day03;
 
+fn find_oxygen(mut input: Vec<Vec<char>>, inverse: bool) -> usize {
+    let mut i = 0;
+
+    while input.len() > 1 {
+        let ones = input.iter().filter(|l| l[i] == '1').count();
+        let zeroes = input.iter().filter(|l| l[i] == '0').count();
+
+        if ones >= zeroes {
+            input.retain(|l| l[i] == if inverse { '1' } else { '0' });
+        } else {
+            input.retain(|l| l[i] == if inverse { '0' } else { '1' });
+        }
+
+        i += 1;
+    }
+
+    usize::from_str_radix(&input[0].iter().collect::<String>(), 2).unwrap()
+}
+
 impl AoC for Day03 {
     type Output = usize;
     type Input = Vec<char>;
@@ -27,7 +46,10 @@ impl AoC for Day03 {
     }
 
     fn part_two(input: &[Self::Input]) -> usize {
-        todo!()
+        let oxygen = find_oxygen(input.to_vec(), true);
+        let scrubber = find_oxygen(input.to_vec(), false);
+
+        oxygen * scrubber
     }
 }
 
@@ -52,16 +74,16 @@ mod day02 {
     }
 
     #[test]
-    #[ignore]
     fn part_2_example() {
-        let data = Day03::parse("forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2");
-        assert_eq!(900, Day03::part_two(&data));
+        let data = Day03::parse(
+            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010",
+        );
+        assert_eq!(230, Day03::part_two(&data));
     }
 
     #[test]
-    #[ignore]
     fn part_2() {
         let data = Day03::parse(INPUT);
-        assert_eq!(1864715580, Day03::part_two(&data));
+        assert_eq!(1032597, Day03::part_two(&data));
     }
 }
