86ef5d69234fc2c86c74faf206d98f06b76aee5f
Sondre Nilsen <sondre@eons.io>
2021-12-05 19:26:31 +0100

    Add day 5 to frontend

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -2,6 +2,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 trait AoC {
     type Output: std::fmt::Display;
@@ -24,6 +25,7 @@ pub fn run_day(day: usize, input: &str) -> (String, String) {
         2 => day02::Day02::solve(input),
         3 => day03::Day03::solve(input),
         4 => day04::Day04::solve(input),
+        5 => day05::Day05::solve(input),
         _ => ("Not implemented".to_string(), "Not implemented".to_string()),
     }
 }
